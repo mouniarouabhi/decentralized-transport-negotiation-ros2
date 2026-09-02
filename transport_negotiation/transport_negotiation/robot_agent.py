ef70cdfb67cfffff5ca41686eade5bce76efae93
Mounia Rouabhi <mm_rouabhi@esi.dz>
2026-09-02 16:38:27 +0100

    Add publisher and subscriber wiring to robot_agent for task announcments and bids

--- transport_negotiation/transport_negotiation/robot_agent.py
+++ transport_negotiation/transport_negotiation/robot_agent.py
@@ -2,11 +2,17 @@
 
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 class RobotAgent(Node):
 	def __init__(self):
 		super().__init__('robot_agent')
 		self.get_logger().info('robot_agent node started')
+		self.task_sub = self.create_subscription(String, '/task_announcements', self.on_task_announcement, 10)
+		self.bid_pub = self.create_publisher(String, '/bids', 10)
+
+	def on_task_announcement(self, msg):
+		self.get_logger().info(f'Received task announcement : {msg.data}')
 
 def main(args=None):
 	rclpy.init(args=args)
